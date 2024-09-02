894db21653f37bb2a9eeb40432c9fcca7cccf527
jackberck <berckjack@gmail.com>
2024-09-02 14:08:23 +0700

    init: project setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to my website!</h1>
+        <p>Feel free to browse around.</p>
+      </div>
+    </>
+  );
+}
